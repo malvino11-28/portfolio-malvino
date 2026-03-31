c76bce49b5062d4d797f5f1794799b53a1dc8e14
Malvino <malvinogarcia2@gmail.com>
2026-03-31 17:16:02 -0600

    feat: atualizar estilos e estrutura da página Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
 export const Home = () => {
-  return <div></div>;
+  return (
+    <div className="min-h-screen">
+      {/*Theme Toggle */}
+
+      {/*Background effects*/}
+
+      {/* Navbar */}
+
+      {/*Main content */}
+
+      {/* Footer */}
+    </div>
+  );
 };
