5290ee42238ee3fdd8d680e9732b08f0e3418498
Malvino <malvinogarcia2@gmail.com>
2026-03-31 17:54:18 -0600

    feat: adiciona funcionalidade de troca de tema da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes } from "react-router-dom";
-import "./App.css";
 import { Route } from "lucide-react";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 export const Home = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/*Theme Toggle */}
-
+      <ThemeToggle />
       {/*Background effects*/}
 
       {/* Navbar */}
