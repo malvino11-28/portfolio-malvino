797d4c4426bd1e9b99ce9d1156ff959822c1a28e
Malvino30-06 <malvinogarcia2@gmail.com>
2026-04-02 13:49:33 -0300

    feat: adicionando componente AboutMe

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,32 @@
+export const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-2xl md:text-4xl font-bold mb-12 text-center">
+          Sobre <span className="text-primary">mim</span>
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Apaixonado por desenvolvimento backend & RPA
+            </h3>
+
+            <p className="text-muter-foreground">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+              sunt nostrum quia quibusdam distinctio in perspiciatis iusto
+              debitis voluptate.
+            </p>
+
+            <p className="text-muter-foreground">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+              sunt nostrum quia quibusdam distinctio in perspiciatis iusto
+              debitis voluptate.
+            </p>
+          </div>
+
+          <div className=""></div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Navbar } from "../components/NavBar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutMe";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -14,6 +15,7 @@ export const Home = () => {
       {/*Main content */}
       <main>
         <HeroSection />
+        <AboutSection />
       </main>
       {/* Footer */}
     </div>
