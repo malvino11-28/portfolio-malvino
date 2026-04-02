2cf7cf20153f796a5c8f72554aa17fd82119003f
Malvino30-06 <malvinogarcia2@gmail.com>
2026-04-02 13:09:39 -0300

    feat: finalizando hero, adicionando botões, icones, e breve informações sobre mim

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "../components/NavBar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
+import { HeroSection } from "../components/HeroSection";
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
