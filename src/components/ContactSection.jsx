00aecb9b4e8b8edfb93ceb06b0a4d176791771b2
Malvino30-06 <malvinogarcia2@gmail.com>
2026-04-02 17:10:55 -0300

    fix: ajuste de posicionamento dos conteúdos de contato e do formulário

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -6,71 +6,74 @@ import { cn } from "@/lib/utils.js";
 export const ContactSection = () => {
   return (
     <section id="contact" className="py-24 px-4 relative bg-secondary/30">
-      <div className="container mx-auto max-w-5xl"></div>
-      <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-        Entre em <span className="text-primary">Contato</span>
-      </h2>
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+          Entre em <span className="text-primary">Contato</span>
+        </h2>
 
-      <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
-        Tem um projeto em mente ou quer colaborar? Sinta-se a vontade para
-        entrar em contato. Estou sempre aberto para novas oportunidades.
-      </p>
+        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+          Tem um projeto em mente ou quer colaborar? Sinta-se a vontade para
+          entrar em contato. Estou sempre aberto para novas oportunidades.
+        </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-        <div className="space-y-8">
-          <h3 className="text-2xl font-semibold mb-6">
-            {" "}
-            Informações de Contato
-          </h3>
-          <div className="space-y-6 justify-center">
-            <div className="flex items-start space-x-4">
-              <div className="p-3 rounded-full bg-primary/10">
-                <Mail className="h-6 w-6 text-primary" />
-              </div>
-              <div>
-                <h4 className="font-medium"> Email</h4>
-                <a
-                  href="mailto:malvinodev2@gmail.com"
-                  className="text-muted-foreground hover:text-primary transition-colors"
-                >
-                  malvinodev2@gmail.com
-                </a>
-              </div>
-            </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-start">
+          {/* Coluna da esquerda */}
+          <div className="space-y-8 flex flex-col items-center w-fit">
+            <h3 className="text-2xl font-semibold mb-8">
+              Informações de Contato
+            </h3>
 
-            <div className="flex items-start space-x-4">
-              <div className="p-3 rounded-full bg-primary/10">
-                <WhatsappIcon className="h-6 w-6 text-primary" />
+            <div className="space-y-16">
+              <div className="flex items-center gap-4 hover:translate-x-1 transition-transform duration-300 md:w-99">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Mail className="h-6 w-6 text-primary" />
+                </div>
+                <div className="md:w-70">
+                  <h4 className="font-medium">Email</h4>
+                  <a
+                    href="mailto:malvinodev2@gmail.com"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    malvinodev2@gmail.com
+                  </a>
+                </div>
               </div>
-              <div>
-                <h4 className="font-medium"> Whatsapp </h4>
-                <a
-                  href="https://api.whatsapp.com/send/?phone=5518996371968&text&type=phone_number&app_absent=0"
-                  className="text-muted-foreground hover:text-primary transition-colors"
-                >
-                  +55 (18) 99637-1968
-                </a>
-              </div>
-            </div>
 
-            <div className="flex items-start space-x-4">
-              <div className="p-3 rounded-full bg-primary/10">
-                <LinkedinIcon className="h-6 w-6 text-primary" />
+              <div className="flex items-center gap-4 hover:translate-x-1 transition-transform duration-300">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <WhatsappIcon className="h-6 w-6 text-primary" />
+                </div>
+                <div className="md:w-70 w-45">
+                  <h4 className="font-medium">Whatsapp</h4>
+                  <a
+                    href="https://api.whatsapp.com/send/?phone=5518996371968&text&type=phone_number&app_absent=0"
+                    className="text-muted-foreground hover:text-primary transition-colors"
+                  >
+                    +55 (18) 99637-1968
+                  </a>
+                </div>
               </div>
-              <div>
-                <h4 className="font-medium"> Linkedin </h4>
-                <a
-                  className="text-muted-foreground hover:text-primary transition-colors"
-                  href="https://www.linkedin.com/in/malvino-garcia"
-                >
-                  https://www.linkedin.com/in/malvino-garcia
-                </a>
+
+              <div className="flex items-center gap-4 hover:translate-x-1 transition-transform duration-300">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <LinkedinIcon className="h-6 w-6 text-primary" />
+                </div>
+                <div className="md:w-70">
+                  <h4 className="font-medium">Linkedin</h4>
+                  <a
+                    className="text-muted-foreground hover:text-primary transition-colors break-all"
+                    href="https://www.linkedin.com/in/malvino-garcia"
+                  >
+                    linkedin.com/in/malvino-garcia
+                  </a>
+                </div>
               </div>
             </div>
           </div>
 
+          {/* Coluna da direita */}
           <div className="bg-card p-8 rounded-lg shadow-xs">
-            <h3 className="text-2xl font-semibold mb-6"> Envie uma mensagem</h3>
+            <h3 className="text-2xl font-semibold mb-6">Envie uma Mensagem</h3>
 
             <form className="space-y-6">
               <div>
@@ -78,7 +81,6 @@ export const ContactSection = () => {
                   htmlFor="name"
                   className="block text-sm font-medium mb-2"
                 >
-                  {" "}
                   Seu nome
                 </label>
                 <input
@@ -86,40 +88,41 @@ export const ContactSection = () => {
                   id="name"
                   name="name"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
                   placeholder="Kauan Malvino..."
                 />
               </div>
+
               <div>
                 <label
                   htmlFor="email"
                   className="block text-sm font-medium mb-2"
                 >
-                  {" "}
                   Seu email
                 </label>
                 <input
-                  type="text"
+                  type="email"
                   id="email"
                   name="email"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary"
                   placeholder="exemplo@exemplo.com"
                 />
               </div>
+
               <div>
                 <label
                   htmlFor="message"
                   className="block text-sm font-medium mb-2"
                 >
-                  {" "}
                   Mensagem
                 </label>
                 <textarea
                   id="message"
                   name="message"
                   required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none"
+                  rows={2}
+                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-none focus:ring-2 focus:ring-primary resize-none"
                   placeholder="Olá..."
                 />
               </div>
@@ -130,7 +133,6 @@ export const ContactSection = () => {
                   "cosmic-button w-full flex items-center justify-center gap-2",
                 )}
               >
-                {" "}
                 Enviar Mensagem
                 <Send size={16} />
               </button>
