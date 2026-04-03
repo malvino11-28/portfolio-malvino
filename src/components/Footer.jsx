963b04954c42be60447b5ffb38f0c7057ebe1943
Malvino <malvinogarcia2@gmail.com>
2026-04-03 12:52:02 -0600

    chore: alterando texto do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
       <p className="text-sm text-muted-foreground">
         {" "}
-        &copy; {new Date().getFullYear} Malvino.co. All rights reserved.
+        &copy; {new Date().getFullYear} © 2026 Kauan Malvino Garcia
       </p>
       <a
         href="#hero"
